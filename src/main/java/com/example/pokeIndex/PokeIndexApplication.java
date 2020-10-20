291e4a3fe2a8a301e8e18afb7d4a584c1f7bc11f
Fredrik <Fredrikjay@gmail.com>
2020-10-20 10:22:18 +0200

    initialize  project

---
+++ src/main/java/com/example/pokeIndex/PokeIndexApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokeIndex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeIndexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pokeIndex/PokeIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokeIndex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeIndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
